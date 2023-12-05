82924ff89655a31021d5a03b9fe166f024d52156
Jackie <jlauravelez@gmail.com>
2023-12-05 20:24:13 +0100

    choosing characters

--- scripts/character.gd
+++ scripts/character.gd
@@ -11,3 +11,13 @@ extends Node
 @export var attacks : Array[attack_types] 
 
 @export var image : Texture
+
+@onready var texture_rect = $Control/VBoxContainer/TextureRect
+
+
+
+func on_hero_hover():
+	texture_rect.modulate = Color(255,255,255,255)
+
+func on_hero_leave():
+	texture_rect.modulate = Color(255,255,255,0)
---
+++ scripts/game.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+var crusader = preload("res://scenes/Heroes/Crusader.tscn")
+
+var arr_of_heroes = [crusader, crusader]
+#var arr_of_enemies = 
+
+
+
