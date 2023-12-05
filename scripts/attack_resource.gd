1e749e9ea3793c7886ec0eba43d452135ad8a76d
Jackie <jlauravelez@gmail.com>
2023-12-05 17:36:30 +0100

    canvas stuff part 1

--- scripts/attack_resource.gd
+++ scripts/attack_resource.gd
@@ -6,7 +6,7 @@ enum Range_Type {Melee, Ranged}
 enum Enemy_Type {UNHOLY, HUMAN, BEAST, STONEWORK, ELDRITCH, BLOODSUCKER, HUSK, OTHER, NONE}
 
 @export var name : String = ""
-@export var texture : AtlasTexture
+@export var texture : Texture
 
 @export var range : Range_Type = Range_Type.Melee
 @export var rank : Vector4 = Vector4.ZERO
